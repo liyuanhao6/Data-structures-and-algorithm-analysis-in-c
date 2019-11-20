75ef3ccbf5a3b2e4b5ce59c127951c8316adc491
Antoine Li <46842302+liyuanhao6@users.noreply.github.com>
2019-11-20 17:33:13 +0800

    Update 算法4.c

--- Chapter 1: Introduction/1 最大子序列和/算法4.c
+++ Chapter 1: Introduction/1 最大子序列和/算法4.c
@@ -5,7 +5,7 @@ int MaxSubsequenceSum( const int A[], int N )
     int ThisSum, MaxSum, i;
 
     ThisSum = MaxSum = 0;
-    for ( i = 0; i < N; i++ )
+    for ( i = 0; i < N; i++ ) // 从i清点到A[i]至总和为正
     {
         ThisSum += A[i];
             

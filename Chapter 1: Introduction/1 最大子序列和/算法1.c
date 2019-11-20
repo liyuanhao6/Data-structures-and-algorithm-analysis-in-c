90daa067edc32fdeb15d75397ad9f53cf801d4d7
Antoine Li <46842302+liyuanhao6@users.noreply.github.com>
2019-11-20 17:27:11 +0800

    Update 算法1.c

--- Chapter 1: Introduction/1 最大子序列和/算法1.c
+++ Chapter 1: Introduction/1 最大子序列和/算法1.c
@@ -5,11 +5,11 @@ int MaxSubsequenceSum( const int A[], int N )
     int ThisSum, MaxSum, i, j, k;
 
     MaxSum = 0;
-    for ( i = 0; i < N; i++ )
-        for ( j = i; j< N; j++ )
+    for ( i = 0; i < N; i++ ) // 基准
+        for ( j = i; j< N; j++ ) // 结尾
         {
             ThisSum = 0;
-            for ( k = i; k <= j; k++ )
+            for ( k = i; k <= j; k++ ) // 清点i到j
                 ThisSum += A[k];
             
             if ( ThisSum > MaxSum )
